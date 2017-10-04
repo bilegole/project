a2530568d3443dc20a6cd681a9e3bb3bf74c189b
ewnf <958102068@qq.com>
2017-10-04 17:14:54 +0800

    数据库基本完成？（吹吧），先准备爬虫部分。

--- 1.py
+++ 1.py
@@ -3,125 +3,12 @@
 import os
 import copy
 
-
-
-
-
-def read_file(file):
-	with open(a, 'w') as f:
-                f.readlines()
-
-
-
-
-
-
-
-
-
-
-#	par_0 = [9,8,30,5,20,7]
-#	bundle = [par_0]		
-#	if os.path.exists('./parameter.cof'):	
-#		with open('./parameter.cof', 'r') as file:
-#    			par_1 =file.read()
-#			bundle = process(par_1,bundle)
-#	return bundle
-
-
-
-
-def load_out(a):
-	
-	len98 = len(a)
-	count = 0
-	while (len(a)>(count+1)):
-		if isinstance(a[count],list):
-			random_test(a[count])
-			count = count + 1
-		else:
-			while (count+1) < len98 :
-				if a[count] != None :
-					a[count].print_self()
-				count = count + 1
-
-
-#****************************************************************************
-#此函数用于写入数据文件
-#****************************************************************************
-
-
-
-
-
-#****************************************************************************
-#此函数用于打开被输入文件夹下的所有文件
-#****************************************************************************
-def read_in(dir):
-	dir = str(dir)
-	now = os.path.abspath('.')
-	then = os.path.join(now,dir)
-	os.chdir(then)
-	dir_1 = [x for x in os.listdir('.') if x !='.DS_Store' ]	
-	len98 = len (dir_1)
-	count = 0
-	while (count<len(dir_1)):
-		if os.path.isdir(dir_1[count]):
-			read_in(dir_1[count])
-			count = count + 1
-			os.chdir(then)
-		else:
-			read_file_in(os.path.join(os.path.abspath('.'),dir_1[count]))
-		count = count  + 1
-
-#****************************************************************************
-#此函数用于读取文件（课）的配置
-#****************************************************************************
-def read_file_in(file_in):
-	#print ('开始read——in——函数')
-	global DATEBASE
-	ev_in = 'ev_in'
-	ev_conf = 'ev_conf'
-	ev_out = 'ev_out'
-	with open(file_in, 'r') as file:
-	        buffer_in = file.readlines()
-	title = buffer_in[0][:-1]
-	buffer_in.pop(0)
-	id_list = []
-	class_tmp = Classt()
-	#print(buffer_in)
-	for i in range(len(buffer_in)) :
-
-		string =  buffer_in[i]
-		judge = string[:3]
-	#	print('judge:',judge)
-		if judge == '001':
-			for letter in string[4:-1]:
-				id_list.append(letter)
-	#		print('id_list:',id_list)
-		elif judge == '002':
-			id_list.insert(2,string[4:-1])	 
-	#		print (string[4:-1])
-#			print (id_list)
-			class_tmp.give_id(id_list)
-		elif judge == '003':
-			name_oc = string[4:-1]
-			class_tmp.give_name(name_oc)
-		elif judge == '004':
-			id_ot = string[4:-1]
-			class_tmp.give_teacher_id(id_ot)
-	rewrite(id_list,class_tmp)
-#	DATEBASE[id_list[0]][id_list[1]][id_list[2]][id_list[3]][id_list[4]][id_list[5]] = class_tmp
-#	class_tmp.print_self()
-
-#def trans(a):
-#	t = []
-#	for i in a :
-#		t.append(i)
-#	return t
 #****************************************************************************
 #这是用于创建高维数组的函数
 #****************************************************************************
+
+
+#以下两个函数用于建立DATEBASE所需的高维数组
 def creat_datebase1(*need):
 	need_1 = list(copy.copy(need))
 	for e in range(len(need)):
@@ -145,7 +32,7 @@ def creat_datebase2(need,mat=0):
 	return bundle
 
 
-#此函数用于写入高维数组，并且使其指针正确
+#此函数用于调换高维数组：DATEBASE的某个变量，并且使其指针正确
 def rewrite(list_id,class_t):
 	t = copy.copy(list_id)
 	for j in range(len(t)):
@@ -156,14 +43,84 @@ def rewrite(list_id,class_t):
 	i_3 = copy.copy(DATEBASE[t[0]][t[1]][t[2]])
 	i_2 = copy.copy(DATEBASE[t[0]][t[1]])
 	i_1 = copy.copy(DATEBASE[t[0]])
-#	print(len(i_5))
-#	print(t)
 	i_5[t[5]] = class_t						#i_5的长度是7
 	i_4[t[4]] = i_5							#i_4的长度是20
 	i_3[t[3]] = i_4							#i_3的长度是5
 	i_2[t[2]] = i_3							#i_2的长度是30
 	i_1[t[1]] = i_2							#i_1的长度是8
 	DATEBASE[t[0]] = i_1          					#DATEBASE的长度是9
+
+#此函数未开始写
+#@@@\\\
+def rewrite_chart():
+	pass		
+
+#这是DATEBASE自检函数，输出每个被赋值的课堂
+def load_out(a):
+	len98 = len(a)
+	count = 0
+	while (len(a)>(count+1)):
+		if isinstance(a[count],list):
+			random_test(a[count])
+			count = count + 1
+		else:
+			while (count+1) < len98 :
+				if a[count] != None :
+					a[count].print_self()
+				count = count + 1
+
+#****************************************************************************
+#读取文本形式存放的数据库
+#****************************************************************************
+
+#调用每一个文本数据
+def read_in(dir):
+	dir = str(dir)
+	now = os.path.abspath('.')
+	then = os.path.join(now,dir)
+	os.chdir(then)
+	dir_1 = [x for x in os.listdir('.') if x !='.DS_Store' ]	
+	len98 = len (dir_1)
+	count = 0
+	while (count<len(dir_1)):
+		if os.path.isdir(dir_1[count]):
+			read_in(dir_1[count])
+			count = count + 1
+			os.chdir(then)
+		else:
+			read_file_in(os.path.join(os.path.abspath('.'),dir_1[count]))
+		count = count  + 1
+#将其写入到内存中
+def read_file_in(file_in):
+	#print ('开始read——in——函数')
+	global DATEBASE
+	ev_in = 'ev_in'
+	ev_conf = 'ev_conf'
+	ev_out = 'ev_out'
+	with open(file_in, 'r') as file:
+	        buffer_in = file.readlines()
+	title = buffer_in[0][:-1]
+	buffer_in.pop(0)
+	id_list = []
+	class_tmp = Classt()
+	#print(buffer_in)
+	for i in range(len(buffer_in)) :
+
+		string =  buffer_in[i]
+		judge = string[:3]
+		if judge == '001':
+			for letter in string[4:-1]:
+				id_list.append(letter)
+		elif judge == '002':
+			id_list.insert(2,string[4:-1])	 
+			class_tmp.give_id(id_list)
+		elif judge == '003':
+			name_oc = string[4:-1]
+			class_tmp.give_name(name_oc)
+		elif judge == '004':
+			na_ot = string[4:-1]
+			class_tmp.give_teacher_str(na_ot)
+	rewrite(id_list,class_tmp)
 #****************************************************************************
 #这两个函数被用于测试数组
 #****************************************************************************
@@ -201,12 +158,8 @@ def random_test(a):
 
 def trans (list_tmp):
 	st = ''
-	
 	for i in list_tmp : 
 		t = str (i)
-		if i == 2:
-			t = '0' + t 
-		print (t)
 		st = st + t
 	return st
 
@@ -216,6 +169,15 @@ def trans (list_tmp):
 #以下是定义类的区域
 #包括:课程，老师
 #****************************************************************************
+
+class DATEBASE(object):
+	
+	def __init__(self)
+		
+
+
+
+
 
 
 
@@ -228,11 +190,10 @@ class Classt(object):
                 self.id_str = None
                 self.name = None
                 self.teacher = None
-                pass
-
-
-
-#	标号规则：以str形式储存，需要时转为int
+		self.score = None	#
+		self.must = None	#
+		self.time = None	#
+#		self.tea_dict = None
 #	目前有6位，最大大小分别为：9,8,30,5,20,7
 #	储存在文本中时，以：
 #	001@abefg
@@ -242,20 +203,20 @@ class Classt(object):
 		self.id_list = id_list
 		self.id_str = trans(self.id_list)
 		print(id_list)
-#		print(self.id)
-#		print(self.id_str)
 	def give_name(self,name):
                  self.name = name
-	def give_teacher_id(self,iden):
-                self.teacher = teacher(iden)
+	def give_teacher_str(self,st):
+                self.teacher = st
+	#	self.tea_dict = read_str(self.teacher)
 		#self.tea_id = iden
 	def print_self(self):
 		print('类classt已创建')
 		print('课程名称为：',self.name)
 		print('id为',self.id_str)
 		print('list:',self.id_list)
-		print('老师的id为',self.teacher.iden)
-	def load_out(self,path):				#此函数需要date的目录（如：／Users/yuyang/project/date ）
+		print('老师的名字为：',self.teacher)
+	#此函数需要date的目录（如：／Users/yuyang/project/date ）
+	def load_out(self,path):			
 		global HOME
 		os.chdir(os.path.join(HOME,path))
 		for count in range(len(self.id_list)-1):
@@ -268,7 +229,6 @@ class Classt(object):
 				os.mkdir(os.path.join(os.path.abspath('.'),str(self.id_list[count])))
 			os.chdir(str(self.id_list[count]))
 		self.write_file()
-
 	def write_file(self):
 		content = []
 		content.append('ev_in\n')
@@ -278,24 +238,97 @@ class Classt(object):
 		if self.name != None :
 			content.append('003@'+self.name+'\n')
 		if self.teacher != None :
-			content.append('004@'+self.teacher.iden+'\n')
+			content.append('004@'+self.teacher+'\n')
 		with open(str(self.id_list[-1]), 'w+') as file:
 			file.writelines(content)
 
+#这里有个问题，要输入的是course_t的id，却输入了老师的字符串
+	def course_out(self):
+		tmp = course_t()
+		tmp.in_id(cla_to_cou(self))
+		tmp.in_tea(read_str(self.teacher))
+		tmp.in_score(self.score)
+		tmp.in_must(self.must)
+		tmp.in_time(self.time)	
+		return tmp
+	#返回一个course_t类型数据
+
+
+
+def read_str(str):
+	pass
+def cla_to_cou(cls):
+	return result		#此处返回用于从classt中生成的course_t的id	
+
+
+
+class Chartt(object):
+	def __init__(self):
+		self.department = None		#指这张课表所属学院
+		self.major = None		#指这张课表所属专业
+		self.class_n = None		#指这张课表所属班级
+	#	self.week = None		#指这张课表所属星期
+		self.mat = None			#这张表所需数据库
+						#1.星期几 2.课次 3.周 
+						#1.7	  2.5	 3.20 	
+	def setup(self):
+		self.datebase = creat_datebase1(7,5,20)
+		
 
 
 
 
 
-class teacher(object):
-	def __init__(self,iden):
-	        self.iden = iden
+
+
+
+
+
+
+
+
+#
+#class teacher(object):
+#	def __init__(self,iden):
+#	        self.iden = iden
+#		self.post = None		
+#		self.name = None
+#	def id_in(self)
+
+#教师编码
+#
+
+
+
 
 date = (10,8,30,4,20,7)
 
-
-
-
+class course_t():
+	def __init__(self):
+		self.id_list = None			#这是指list形式的id
+		self.id_str = None			#这是指string形式的id
+		self.tea_list = []			#这是指老师的列表
+		self.tea_num = len(self.tea_list)	#这是指老师的数量
+		self.score = None			#这是指course_t的学分
+		self.must = None			#这是指course_t是否为必修课
+		self.time = None			#这是指course_t的学时
+	def in_id(self,lst):
+		self.id_list = lst
+		self.id_str = trans(self.id_list)	
+	def in_tea():
+		pass
+	def in_score(self,num):
+		self.score = num
+	def in_must(self,must):
+		self.must = must
+	def in_time(self,time):
+#课程编码
+#校区：1:南校区	2:北校区0:特殊	
+#周几：（1-7）星期几
+#课次：1:1-2	2:3-4	3:5-6	4:7-8	0:晚自修
+#教室：同DATEBASE编码
+#楼层：1-8
+#教室：0-30
 
 
 
@@ -305,6 +338,12 @@ date = (10,8,30,4,20,7)
 
 
 def main():
+#	HOME = os.path.abspath('.')
+#	DATEBASE = creat_datebase1(9,8,30,5,20,7)
+#	read_in('date')
+#	random_test(DATEBASE)
+#	os.chdir(HOME)
+#	print('程序执行完毕')		
 	pass
 
 building = {'A':1,'B':2,'C':3,'D':4,'E':5,'信远楼':6,'工训中心':7,'大学生活动室':8,'北操场':9,'南操场':10}
@@ -318,15 +357,10 @@ department = {'计算机':1,'通信工程':2,'电子工程':3,'机电工程':4,'
 #______________________________________________________________________________
 
 
-#现在在调试数组的空间大小，程序总是说数组越界。。。。。
-#去他妈的
 
-#bundle = read_parameter()
 
 HOME = os.path.abspath('.')
-
 DATEBASE = creat_datebase1(9,8,30,5,20,7)
-#DATEBASE[1][1][4][1][1][1] = Classt()
 read_in('date')
 random_test(DATEBASE)
 os.chdir(HOME)
@@ -338,7 +372,7 @@ ii.give_id(ii_id)
 print('bbb')
 ii.give_name('算法导论')
 print ('ccc')
-ii.give_teacher_id('teacher_id')
+ii.give_teacher_str('teacher')
 print (ii.name)
 ii.print_self()
 ii.load_out('date')
